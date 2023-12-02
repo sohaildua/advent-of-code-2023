cee046a5ecac8220776060eb2785424238daf6ce
sdgamer007 <sohaildua38@gmail.com>
2023-12-02 01:11:12 +0100

    first part day 1 sol1

---
+++ day01/sol1.py
@@ -0,0 +1,15 @@
+import re
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    data = [entry.strip() for entry in lines]
+    
+    print(data)
+    result = 0
+    for value in data:
+        tens_number =re.search(r'[\d]{1}', value).group()
+        digit_number = re.search(r'[\d]{1}', value[::-1]).group()
+        
+        total_digit_number = int(tens_number + digit_number) 
+        result += total_digit_number
+    print(result)
+        
