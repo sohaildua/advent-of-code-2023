607ed500279b3b2f5201c21f8d78255e309b11da
sdgamer007 <sohaildua38@gmail.com>
2023-12-03 19:23:14 +0100

    logic part 1 calculation done

--- day02/sol1.py
+++ day02/sol1.py
@@ -1,33 +1,36 @@
+
 def gamefunctions(str):  
+        count = 0
         valSplit = str.split(':')
-        listngame = []
         gameNumber = int(valSplit[0].split(' ')[1].strip())
         valBallSplit = valSplit[1].split(';')
-        blue = 0
-        red = 0
-        green = 0 
         for x in valBallSplit:
+            blue =0
+            red = 0
+            green = 0
             for data in x.split(','):
+                print(len(x.split(',')))
                 if 'blue' in data:
-                    blue += int(data.strip().split(" ")[0])
+                    blue = int(data.strip().split(" ")[0])
                 if 'green' in data:
-                    green += int(data.strip().split(" ")[0])
+                    green = int(data.strip().split(" ")[0])
                 if 'red' in data:
-                    red += int(data.strip().split(" ")[0])
-        listngame.append({"blue": blue})
-        listngame.append({"green": green})
-        listngame.append({"red": red})
-        dictgame[gameNumber] = listngame
-        return dictgame           
-                
+                    red = int(data.strip().split(" ")[0])
+            if blue<=14 and green<=13 and red<=12:
+                count = count + 1
+                if count == len(valBallSplit):
+                    return gameNumber
+            
+        return 0                
     
                 
 with open('day02/input.txt', 'r') as f:
+    result = 0
     lines = f.readlines()
     data = [entry.strip() for entry in lines]
     dictgame = {}
     for x in data:
-        dictgame = gamefunctions(x)
+       result += gamefunctions(x)
     
-print(dictgame)
+    print(result)
 
